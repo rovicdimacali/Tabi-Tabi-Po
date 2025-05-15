91e60750f927408df236f5fe907cc900e5fbd800
Rovic Dimacali <rovic.dimacali@dexterton.com>
2025-05-15 23:37:14 +0800

    fixed title tutorial

--- src/components/home/tutorial.vue
+++ src/components/home/tutorial.vue
@@ -1,6 +1,6 @@
 <template>
   <section class="tutorial" id="gameplay">
-    <h1>How to play the Tabi Tabi Po Collapsible Board Game</h1>
+    <h1>Game Mechanics</h1>
     <iframe
       style="width: 100%; aspect-ratio: 16 / 9"
       src="https://www.youtube.com/embed/H8MjcJ1aaQI?si=kW-1PuYG7LiRxrsk"
